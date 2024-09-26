ff5c855295d81895e82dd461ff8c6ef531f12127
Balnur Bektassova <31367887+b-bektassova@users.noreply.github.com>
2024-09-26 13:05:58 +0500

    Add files via upload

---
+++ assignment3-total/iamrich/iamrich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by Балнур Бектасова on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
